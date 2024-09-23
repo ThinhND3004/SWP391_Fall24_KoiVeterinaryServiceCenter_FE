00c6dffa25589b49597f20b19eeaf0d6ad32d4c3
hnglmiee <lambph1204@gmail.com>
2024-09-23 22:39:56 +0700

    add intro

--- src/pages/ServicePage/ServicePageDetails/Introduction.jsx
+++ src/pages/ServicePage/ServicePageDetails/Introduction.jsx
@@ -7,7 +7,7 @@ function Introduction() {
     <div>
       <Typography sx={{ fontFamily: 'SVN-Konga Pro', fontSize: 120, textAlign: 'center', color: BLUE_COLOR }}>Our Medical Services</Typography>
       <Typography sx={{ textAlign: 'center', fontSize: 40, marginTop: '10px', fontWeight: 700 }}>
-        You deserve <span style={{ color: ORANGE_COLOR }} >support,</span>not silos
+        You deserve <span style={{ color: ORANGE_COLOR }} >support,</span> not silos
       </Typography>
       <Typography sx={{ textAlign: 'center', fontSize: 14, marginTop: '10px' }}>
         Complete. Connected. Compassionate. At Tia, we care for the whole you with a team of<br /> providers working together to help you achieve optimal health, on your terms.
