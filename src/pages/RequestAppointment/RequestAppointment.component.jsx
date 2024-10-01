00fdcb1666dcd7c878ef102f1feddc9171fe75cf
hnglmiee <lambph1204@gmail.com>
2024-10-01 20:30:32 +0700

    request page update

--- src/pages/RequestAppointment/RequestAppointment.component.jsx
+++ src/pages/RequestAppointment/RequestAppointment.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Title from './RequestAppointmentDetails/Title';
 import InputField from './RequestAppointmentDetails/InputField';
+import { Box } from '@mui/material';
 
 function RequestAppointment() {
   return (
     <div>
+      <Box display={'flex'} flexDirection={'column'} gap={'100px'} px={'30px'}>
       <Title/>
       <InputField/>
+      </Box>
     </div>
   );
 }
