2f0add48e9b686c262b734ac3710f15b5bcbe3c9
hnglmiee <lambph1204@gmail.com>
2024-10-05 02:01:13 +0700

    change navbar fontsize

--- src/pages/AdminPage/AdminPageDetails/Navbar.jsx
+++ src/pages/AdminPage/AdminPageDetails/Navbar.jsx
@@ -24,45 +24,99 @@ function Navbar() {
         }}
       >
         <List>
-          <Box sx={{ display: 'flex', alignItems: 'center', color: BLUE_COLOR }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', color: '#000' }}>
             <AccountCircleIcon />
             <ListItem button component={Link} to="#" >
-              <ListItemText primary="Account" />
+              <ListItemText
+                primary="Account"
+                primaryTypographyProps={{
+                  sx: {
+                    fontSize: '18px',
+                    color: '#000',
+                    fontWeight: 500
+                  }
+                }}
+              />
             </ListItem>
           </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'center', color: BLUE_COLOR, paddingTop: '10px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', color: '#000', paddingTop: '10px' }}>
             <LockIcon />
-            <ListItem button component={Link} to="#" sx={{ color: BLUE_COLOR }}>
-              <ListItemText primary="Password" />
+            <ListItem button component={Link} to="#" sx={{ color: '#000' }}>
+              <ListItemText
+                primary="Password"
+                primaryTypographyProps={{
+                  sx: {
+                    fontSize: '18px',
+                    color: '#000',
+                    fontWeight: 500
+                  }
+                }}
+              />
             </ListItem>
           </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'center', color: BLUE_COLOR, paddingTop: '10px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', color: '#000', paddingTop: '10px' }}>
             <SupportAgentIcon />
-            <ListItem button component={Link} to="#" sx={{ color: BLUE_COLOR }}>
-              <ListItemText primary="Customer" />
+            <ListItem button component={Link} to="#" sx={{ color: '#000' }}>
+              <ListItemText
+                primary="Customer"
+                primaryTypographyProps={{
+                  sx: {
+                    fontSize: '18px',
+                    color: '#000',
+                    fontWeight: 500
+                  }
+                }}
+              />
             </ListItem>
           </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'center', color: BLUE_COLOR, paddingTop: '10px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', color: '#000', paddingTop: '10px' }}>
             <CalendarMonthIcon />
-            <ListItem button component={Link} to="#" sx={{ color: BLUE_COLOR }}>
-              <ListItemText primary="Booking" />
+            <ListItem button component={Link} to="#" sx={{ color: '#000' }}>
+              <ListItemText
+                primary="Booking"
+                primaryTypographyProps={{
+                  sx: {
+                    fontSize: '18px',
+                    color: '#000',
+                    fontWeight: 500
+                  }
+                }}
+              />
             </ListItem>
           </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'center', color: BLUE_COLOR, paddingTop: '10px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', color: '#000', paddingTop: '10px' }}>
             <LocalHospitalIcon />
-            <ListItem button component={Link} to="#" sx={{ color: BLUE_COLOR }}>
-              <ListItemText primary="Veterian" />
+            <ListItem button component={Link} to="#" sx={{ color: '#000' }}>
+              <ListItemText
+                primary="Veterian"
+                primaryTypographyProps={{
+                  sx: {
+                    fontSize: '18px',
+                    color: '#000',
+                    fontWeight: 500
+                  }
+                }}
+              />
             </ListItem>
           </Box>
 
-          <Box sx={{ display: 'flex', alignItems: 'center', color: BLUE_COLOR, paddingTop: '10px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', color: '#000', paddingTop: '10px' }}>
             <ArticleIcon />
-            <ListItem button component={Link} to="#" sx={{ color: BLUE_COLOR }}>
-              <ListItemText primary="Prescription" />
+            <ListItem button component={Link} to="#" sx={{ color: '#000' }}>
+              <ListItemText
+                primary="Prescription"
+                primaryTypographyProps={{
+                  sx: {
+                    fontSize: '18px',
+                    color: '#000',
+                    fontWeight: 500
+                  }
+                }}
+              />
             </ListItem>
           </Box>
 
@@ -70,7 +124,16 @@ function Navbar() {
           <Box sx={{ display: 'flex', alignItems: 'center', color: ORANGE_COLOR, paddingTop: '10px' }}>
             <LoginIcon />
             <ListItem button component={Link} to="#" sx={{ color: ORANGE_COLOR, paddingTop: '10px' }}>
-              <ListItemText primary="Logout" />
+              <ListItemText
+                primary="Logout"
+                primaryTypographyProps={{
+                  sx: {
+                    fontSize: '18px',
+                    color: ORANGE_COLOR,
+                    fontWeight: 500
+                  }
+                }}
+              />
             </ListItem>
           </Box>
         </List>
