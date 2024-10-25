43818978d220e73af32ec65ae2cc497f79233c12
nguyendaithinh3004 <nguyendaithinh3004@gmail.com>
2024-10-25 22:19:51 +0700

    login gg handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter } from "react-router-dom"
 import RouteComponent from "./routes"
 import NotificationHandler from "./components/NotificationHandler"
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 function App() {
 
   return (
     <>
-    {/* <div>
+      {/* <div>
       <NotificationHandler />
     </div> */}
-    <BrowserRouter>
-        <RouteComponent />
-      </BrowserRouter></>
+      <GoogleOAuthProvider clientId="510907958584-4eatd4rjtntoiokfc352dds2q9ad7av0.apps.googleusercontent.com">
+        <BrowserRouter>
+          <RouteComponent />
+        </BrowserRouter>
+    </GoogleOAuthProvider >
+    </>
   )
 }
 
