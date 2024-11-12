620a304ae4aee2b321f667ce71e8d084cf778a6b
nguyendaithinh3004 <nguyendaithinh3004@gmail.com>
2024-11-12 19:28:32 +0700

    home api

--- src/api/HomeApi.jsx
+++ src/api/HomeApi.jsx
@@ -3,7 +3,7 @@ import api from "~/config/axios";
 export default class HomeApi {
     static async getServices() {
         try {
-            const response = await api.get(`http://localhost:8089/services`);            
+            const response = await api.get(`services`);            
             return response.data.data;
         } catch (err) {
             console.log(err.message);
